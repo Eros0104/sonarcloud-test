41f5e17ad5ccc5b25c9e25b07db8932de07ab7ff
Eros0104 <erosmendonca@outlook.com>
2024-11-27 15:54:23 +0000

    subtract

--- index.test.js
+++ index.test.js
@@ -1,9 +1,11 @@
-const add = require('./index');
+const { 
+  add,
+} = require('./index');
 
 test('adds 1 + 2 to equal 3', () => {
-    expect(add(1, 2)).toBe(3);
+  expect(add(1, 2)).toBe(3);
 });
 
 test('adds -1 + -1 to equal -2', () => {
-    expect(add(-1, -1)).toBe(-2);
+  expect(add(-1, -1)).toBe(-2);
 });
